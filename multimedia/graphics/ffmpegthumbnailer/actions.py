27dee04a82fe0d1a27f5cd8bb915e67e3ef014b1
poyraz76 <ergunsalman@hotmail.com>
2016-09-02 15:44:12 +0300

    güncelleme

--- multimedia/graphics/ffmpegthumbnailer/actions.py
+++ multimedia/graphics/ffmpegthumbnailer/actions.py
@@ -4,20 +4,22 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 
 def setup():
-    pisitools.dosed("libffmpegthumbnailer/pngwriter.cpp", "#include <cassert>", "#include <cassert>\n#include <cstring>")
-    autotools.configure("--disable-static \
-                         --enable-png \
-                         --enable-jpeg")
+    cmaketools.configure("\
+	                     -DCMAKE_BUILD_TYPE=Release \
+	                     -DCMAKE_INSTALL_PREFIX=/usr \
+                         -DCMAKE_INSTALL_LIBDIR=lib \
+                         -DENABLE_GIO=ON \
+                         -DENABLE_THUMBNAILER=ON")
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.install()
+    cmaketools.install()
 
     # Empty files: NEWS, TODO
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
