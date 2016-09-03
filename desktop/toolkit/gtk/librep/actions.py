4399d55eef7ca52664141060068a0c45aeb3d06e
poyraz76 <ergunsalman@hotmail.com>
2016-09-03 15:15:27 +0300

    ekleme

---
+++ desktop/toolkit/gtk/librep/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+
+def setup():
+    shelltools.system("./autogen.sh \
+                         --prefix=/usr\
+                         --libexecdir=/usr/lib \
+                         --disable-static")
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("NEWS", "TODO", "ChangeLog", "COPYING", "README")
