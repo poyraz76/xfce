8a646c0e6a1a858e453df3632f3bf2b9e55a71cc
poyraz76 <ergunsalman@hotmail.com>
2016-09-05 17:17:44 +0300

    blueman bagımlılık

---
+++ hardware/bluetooth/obexd/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.system("sed -i 's/#include <string.h>/&\n#include <stdio.h>/' plugins/mas.c")
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-server \
+                         --disable-dependency-tracking")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README", "doc/client-api.txt")
